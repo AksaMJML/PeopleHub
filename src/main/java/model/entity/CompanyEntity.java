7bf44b18a9f9485941ef750e3586a4a90909766e
AksaMJML <akaks0186@gmail.com>
2026-02-01 00:03:42 +0530

    Add CompanyEntity class with fields and Lombok annotations

---
+++ src/main/java/model/entity/CompanyEntity.java
@@ -0,0 +1,21 @@
+package model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class CompanyEntity {
+    private String Id;
+    private String name;
+    private String email;
+    private String landLine;
+    private String address;
+    private String city;
+    private String registrationDate;
+}
